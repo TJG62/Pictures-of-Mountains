25cd5abc7e8686c41df448f1474b3381d07f496c
TJG62 <43542691+TJG62@users.noreply.github.com>
2019-06-23 13:32:20 -0400

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,19 @@
+$(".everest").click(function() {
+    showPicture("https://user-images.githubusercontent.com/14214/42292528-246b57fe-7fa1-11e8-83fb-3a0daa76d08e.png");
+    
+});
+
+$(".k2").click(function() {
+    showPicture("https://user-images.githubusercontent.com/14214/42292614-ba367638-7fa1-11e8-8e94-97bc8b566232.png");
+});
+
+$(".kangchenjunga").click(function() {
+    
+    showPicture("https://user-images.githubusercontent.com/14214/42292634-e0fedbac-7fa1-11e8-9d56-6d12dcd7ab3c.png");
+   
+});
+function showPicture(picture) {
+     $(".mountain-picture").attr("src", picture);
+    
+    
+}
